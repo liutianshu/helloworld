6b99efe6012dcdcc3b1ba493e81e8c08b0bef6ef
liutianshu <star@starmacbookair.local>
2015-10-22 14:21:44 +0800

    versiong1.0

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by star on 15/10/22.
+//  Copyright (c) 2015年 star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloworld
+//
+//  Created by star on 15/10/22.
+//  Copyright (c) 2015年 star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
